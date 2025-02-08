938859e7130dc12f124c5e406ddfdcee02bb20ae
Suman Modak <sumanmodak578@gmail.com>
2025-02-08 22:10:01 +0530

    little exercises in js

---
+++ JavaScript/bootcampProfile.js
@@ -0,0 +1,20 @@
+// String - "", '', ``
+let name = "Suman";
+// Number - integer, float
+let age = 24.3;
+// Boolean - true, false
+let isPaid = true;
+
+let favouriteClass = null; // null
+let hometown; // undefined
+
+let skills = ["HTML", "CSS", "JS"]; // array
+
+// Object
+let studentProfile = {
+  name: "suman",
+  age: 24,
+  isPaid: true,
+  skills: ["html", "css", "js"],
+  favouriteClass: null,
+};
