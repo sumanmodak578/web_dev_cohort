0415fdbc7c91ba5acb322b2d0f1dfc1fca0380e7
Suman Modak <sumanmodak578@gmail.com>
2025-02-21 17:33:02 +0530

    some practice

--- JavaScript/JS_Basics/hello.js
+++ JavaScript/JS_Basics/hello.js
@@ -10,6 +10,10 @@ function addTwo(num1, num2) {
   return num1 + num2;
 }
 
-printChai();
-bringBrush(4);
-console.log(addTwo(2, 4));
+// printChai();
+// bringBrush(4);
+// console.log(addTwo(2, 4));
+
+const n1 = 17;
+const n2 = 4;
+console.log(4 % 2);
